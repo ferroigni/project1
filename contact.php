94c6d56e7611dec742a7122673bd9ee77dac202b
ferroigni <ferroigni@gmail.com>
2017-12-14 22:29:13 +0200

    contact.php file is added and nb folder is updated

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Contact Us</title>
+    </head>
+    <body>
+        <form name="contact-form" action="/">
+            <input type="text" name="fname" id="firstname" />
+            <input type="text" name="lname" id="lastname" />
+        </form>
+    </body>
+</html>
